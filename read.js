ea143ffb0fa3691f4bea9da712346bf97acae6bf
MiguelAngel <mimaji@gmail.com>
2023-09-25 08:43:02 +0200

    Leyendo, posicionando,rotando

--- read.js
+++ read.js
@@ -1,18 +1,10 @@
-
-var nIntervId;
-var mark = [];
-var AIS = new L.LayerGroup();
-///var OTRO = new L.LayerGroup();
-let constlatitude=0;
-let control1=true;
-///otro([35.9516516666667,-5.56096833333333])
-///otro([35.96516666667, -5.70])
+var nIntervId, mark = [], AIS = new L.LayerGroup, OTRO = new L.LayerGroup; let constlatitude = 0, control = !0, cuentadatos = 0, forcrear = 0;
 L.Control.Layers.include({
-  getOverlays: function() {
+  getOverlays: function () {
     var control, layers;
     layers = {};
     control = this;
-    control._layers.forEach(function(obj) {
+    control._layers.forEach(function (obj) {
       var layerName;
       if (obj.overlay) {
         layerName = obj.name;
@@ -22,40 +14,45 @@ L.Control.Layers.include({
     return layers;
   }
 });
-
 function interv() {
   loadDoc();
-  // comprobar si ya se ha configurado un intervalo
   if (!nIntervId) {
-    nIntervId = setInterval(function () { loadDoc(); }, 10000);
+    nIntervId = setInterval(function () { loadDoc(); }, 60000);
   }
 }
-function loadDoc() {
-  if (mark.length > 0) {
-    for (i = 0; i <= mark.length - 1; i++) {
-      del(i)
+function loadDoc(){let t=new XMLHttpRequest;t.open("GET","AIS.json",!0),t.send(),t.onreadystatechange=function(){if(4==this.readyState&&200==this.status){let t=JSON.parse(this.responseText);for(let e of t)cuentadatos+=1;if(cuentadatos>0){if(mark.length>0){for(i=0;i<=mark.length-1;i++)del(i);mark=[]}for(let o of t)forcrear+=1,add(o.lat,o.lon,o.cors)}cuentadatos=0}}}
+var customPopup = "";
+var customOptions =
+    {
+    'maxWidth': '400',
+    'width': '200',
+    'className' : 'popupCustom'
     }
-    mark = [];
-  }
-add([35.9516516666667, constlatitude+-5.56096833333333])
-add([35.96516666667, constlatitude+-5.70])
-constlatitude=constlatitude+0.2
+
+function add(lat, long, course) {
+  var marker = L.marker([lat, long], { icon: userIcon, rotationAngle: course - 90 }).addTo(AIS);
+  marker.bindPopup('<div class="cabecera">DATOS AIS</div>'+'<br>'+'<div class="L1">'+'<br><b>Posicion: </b>' + [lat, long]+'</div>'+'<div class="L2">'+'<br><b>Rumbo: </b>'+'<br>'+course+'</br>'+'</div>',customOptions)
+  mark.push(marker)
 }
-function add(coordinates) {
-var marker = L.marker(coordinates).addTo(AIS);
-marker.bindPopup('Position:     '+coordinates)
-mark.push(marker)
-}
-function otro(coordinates) {
-  var marker = L.marker(coordinates).addTo(OTRO);
-  marker.bindPopup('Position:     '+coordinates)
-  //mark.push(marker)
-  }
+userIcon=L.icon({iconUrl:"/img/Vessel.png",iconSize:[27,25],iconAnchor:[20,10],popupAnchor:[-3,-20]});
 function del(i) {
   AIS.removeLayer(mark[i])
 }
 function SinterOp() {
   clearInterval(nIntervId);
-  // liberar nuestro inervalId de la variable
   nIntervId = null;
 }
+(function () {
+var proto_initIcon=L.Marker.prototype._initIcon,proto_setPos=L.Marker.prototype._setPos,oldIE="msTransform"===L.DomUtil.TRANSFORM;
+  L.Marker.addInitHook(function () {
+    var iconOptions = this.options.icon && this.options.icon.options;
+    var iconAnchor = iconOptions && this.options.icon.options.iconAnchor;
+    if (iconAnchor) {
+      iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px');
+    }
+    this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom';
+    this.options.rotationAngle = this.options.rotationAngle || 0;
+    this.on('drag', function (e) { e.target._applyRotation(); });
+  });
+  L.Marker.include({_initIcon:function(){proto_initIcon.call(this)},_setPos:function(t){proto_setPos.call(this,t),this._applyRotation()},_applyRotation:function(){this.options.rotationAngle&&(this._icon.style[L.DomUtil.TRANSFORM+"Origin"]=this.options.rotationOrigin,oldIE?this._icon.style[L.DomUtil.TRANSFORM]="rotate("+this.options.rotationAngle+"deg)":this._icon.style[L.DomUtil.TRANSFORM]+=" rotateZ("+this.options.rotationAngle+"deg)")},setRotationAngle:function(t){return this.options.rotationAngle=t,this.update(),this},setRotationOrigin:function(t){return this.options.rotationOrigin=t,this.update(),this}});
+})();
