007b1e77d7b85d5983e18a747d8604bab20c5fff
MiguelAngelMJ <142404927+MiguelAngelMJ@users.noreply.github.com>
2023-10-03 20:12:51 +0200

    masCampos read.js

--- read.js
+++ read.js
@@ -20,7 +20,7 @@ function interv() {
     nIntervId = setInterval(function () { loadDoc(); }, 60000);
   }
 }
-function loadDoc(){const t=new XMLHttpRequest;t.open("GET","AIS.json",!0),t.send(),t.onreadystatechange=function(){if(4==this.readyState&&200==this.status){let t=JSON.parse(this.responseText);for(let e of t)cuentadatos+=1;if(cuentadatos>0){if(mark.length>0){for(i=0;i<=mark.length-1;i++)del(i);mark=[]}for(let e of t)forcrear+=1,add(e.lat,e.lon,e.cors)}cuentadatos=0}}}
+function loadDoc(){const t=new XMLHttpRequest;t.open("GET","AIS.json",!0),t.send(),t.onreadystatechange=function(){if(4==this.readyState&&200==this.status){let t=JSON.parse(this.responseText);for(let e of t)cuentadatos+=1;if(cuentadatos>0){if(mark.length>0){for(i=0;i<=mark.length-1;i++)del(i);mark=[]}for(let e of t)forcrear+=1,add(e.mmsi,e.lat,e.lon,e.cors,e.speed,e.heading,e.timestamp,e.name,e.imo,e.shiptype,e.callsign)}cuentadatos=0}}}
 var customPopup = "";
 
 // specify popup options 
@@ -32,7 +32,7 @@ var customOptions =
     }
 // bind the custom popup 
 
-function add(o,i,c){var n=L.marker([o,i],{icon:userIcon,rotationAngle:c-90}).addTo(AIS);n.bindPopup('<div class="cabecera">DATOS AIS</div><br><div class="L1"><br><b>Posicion: </b>'+[o,i]+'</div><div class="L2"><br><b>Rumbo: </b><br>'+c+"</br></div>",customOptions),mark.push(n)}userIcon=L.icon({iconUrl:"Vessel.png",iconSize:[27,25],iconAnchor:[20,10],popupAnchor:[-3,-20]});
+function add(t,r,d,b,o,n,e,i,a,p,c){var s=L.marker([r,d],{icon:userIcon,rotationAngle:b-90}).addTo(AIS);s.bindPopup("<table><tr><th>DATOS AIS</th></tr><tr><td><b>Mmsi:  </b>"+[t]+"</td></tr><tr><td><b>Latitude:  </b>"+[r]+"</td></tr><tr><td><b>Longitude:  </b>"+[d]+"</td></tr><tr><td><b>Course:  </b>"+[b]+"</td></tr><tr><td><b>Speed:  </b>"+[o]+"</td></tr><tr><td><b>Heading:  </b>"+[n]+"</td></tr><tr><td><b>Timestamp:  </b>"+[e]+"</td></tr><tr><td><b>Name:  </b>"+[i]+"</td></tr><tr><td><b>Imo:  </b>"+[a]+"</td></tr><tr><td><b>ShipType:  </b>"+[p]+"</td></tr><tr><td><b>Callsign:  </b>"+[c]+"</td></tr></table>",customOptions),mark.push(s)}userIcon=L.icon({iconUrl:"Vessel.png",iconSize:[27,25],iconAnchor:[20,10],popupAnchor:[-3,-20]});
 
 function del(i) {
   AIS.removeLayer(mark[i])
